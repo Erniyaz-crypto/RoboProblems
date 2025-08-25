83ef6dc22445999b10756461963676d07fc101ae
Erniyaz <clearn681@gmail.com>
2025-08-25 14:06:10 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def whatDay(a):
+    numOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+
+
+
+
+m = int(input())
+k = int(input())
+kls = list(map(str,input().split()))
+b = int(input())
+bls = list(map(str,input().split()))
